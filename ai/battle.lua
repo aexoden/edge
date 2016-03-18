60989cc8140f4ba059160d2bbb50e7deba5ad91d
Jason Lynch <jason@calindora.com>
2016-03-17 19:55:45 -0500

    Ensure that Edge is elixired before FuSoYa casts Virus.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ai/battle.lua
+++ ai/battle.lua
@@ -946,7 +946,7 @@ local function _battle_grind(character, turn)
 							_command_use_item(game.ITEM.ITEM.ELIXIR, menu.battle.TARGET.CHARACTER, game.CHARACTER.FUSOYA)
 						elseif game.enemy.get_stat(0, "hp") > 50 then
 							_command_black(game.MAGIC.BLACK.WEAK)
-						elseif _state.duplicated then
+						elseif _state.duplicated and _state.edge_elixired then
 							local alive = true
 
 							for i = 0, 4 do
@@ -966,8 +966,9 @@ local function _battle_grind(character, turn)
 							_command_parry()
 						end
 					elseif character == game.CHARACTER.ROSA then
-						if game.character.get_stat(game.CHARACTER.EDGE, "hp", true) < 600 then
+						if game.character.get_stat(game.CHARACTER.EDGE, "hp", true) < 750 then
 							_command_use_item(game.ITEM.ITEM.ELIXIR, menu.battle.TARGET.CHARACTER, game.CHARACTER.EDGE)
+							_state.edge_elixired = true
 						else
 							_command_parry()
 						end
