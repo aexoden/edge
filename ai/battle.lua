1c5e5dc2f6629ce08ee0de8a8405400504a888df
Jason Lynch <jason@calindora.com>
2016-12-04 17:26:19 -0600

    Make the grind fight more robust by trying not to kill more dragons than needed.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- ai/battle.lua
+++ ai/battle.lua
@@ -1018,14 +1018,20 @@ local function _battle_grind(character, turn)
 					_command_parry()
 				end
 			elseif _state.character_index == 2 then
-				_command_use_item(game.ITEM.ITEM.LIFE, menu.battle.TARGET.ENEMY, 1)
+				if dragon_kills < required_dragons - 1 then
+					_command_use_item(game.ITEM.ITEM.LIFE, menu.battle.TARGET.ENEMY, 1)
+				else
+					_command_parry()
+				end
 			elseif _state.character_index == 3 then
 				if dragon_hp > 50 and dragon_hp < 15000 then
 					_command_fight()
 				elseif game.enemy.get_stat(0, "hp") < 800 then
 					_command_use_item(game.ITEM.ITEM.ELIXIR, menu.battle.TARGET.ENEMY, 0)
-				else
+				elseif dragon_kills < required_dragons - 1 then
 					_command_use_item(game.ITEM.ITEM.LIFE, menu.battle.TARGET.ENEMY, 1)
+				else
+					_command_parry()
 				end
 			elseif _state.character_index == 4 then
 				if _state.waited then
