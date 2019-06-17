0304338e5337eff3b1644b3b14a0ecd68a094aa4
Jason Lynch <jason@calindora.com>
2019-06-17 09:26:30 -0700

    Fix some indentation issues with the analyzer.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- main.lua
+++ main.lua
@@ -44,7 +44,7 @@ INITIALIZED = false
 --------------------------------------------------------------------------------
 
 local function _get_version()
-	return "v0.0.8-144"
+	return "v0.0.8-145"
 end
 
 local function _set_route()
