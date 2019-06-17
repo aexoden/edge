0bb26a943b1d5e4ac1088d41d542126a190ec0dc
Jason Lynch <jason@calindora.com>
2019-06-17 10:01:08 -0700

    Fix the detection of chocobo/fireclaw resets in the analyzer.
    Signed-off-by: Jason Lynch <jason@calindora.com>

--- main.lua
+++ main.lua
@@ -44,7 +44,7 @@ INITIALIZED = false
 --------------------------------------------------------------------------------
 
 local function _get_version()
-	return "v0.0.8-147"
+	return "v0.0.8-148"
 end
 
 local function _set_route()
